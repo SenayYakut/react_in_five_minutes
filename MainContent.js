ed360fba3651a5b1ca7a06c43c7d56937eba44fb
Senay Yakut <senaykt@gmail.com>
2019-07-16 17:15:55 -0700

    more component , parent child approach

---
+++ MainContent.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MainContent(){
+   return(
+    <main>
+        <p>This is where we will put our main content of app</p>
+    </main>
+   )
+}
+
+export default MainContent
--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import App from "./App"
 
-import MyInfo from "./components/MyInfo"
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Bob Ziroll</h1>
-      <p>This is a paragraph about me...</p>
-      <ul>
-        <li>Thailand</li>
-        <li>Japan</li>
-        <li>Nordic Countries</li>
-      </ul>
-    </div>
-  )
-}
 
-ReactDOM.render(<MyInfo />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
