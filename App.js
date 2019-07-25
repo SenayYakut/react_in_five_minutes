41df6fa645e642af728e1efcb45063829c7ddb43
Senay Yakut <senaykt@gmail.com>
2019-07-25 16:37:42 -0700

    using differenr approach

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react"
+/*import React from "react"
 import Footer from "./Footer"
 import Header from "./Header"
 import MainContent from "./MainContent"
@@ -14,3 +14,48 @@ function App() {
   }
 
   export default App
+  */
+import React from "react";
+import React, {Component} from 'react';
+import './App.css';
+import {Color, Animal} from './Shapes';
+
+ require('./App.css');
+ var Color = require('./Shapes');
+
+ 
+ class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <div className="colors">
+          <Color name="red"/>
+          <Color name="green"/>
+          <Color name="blue"/>
+        </div>
+      </div>
+    );
+  }
+}
+module.exports=App;
+
+var {Foo} = require('./Shapes');
+var {Bar} = require('./Shapes');
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="colors">
+          <Foo name="red"/>
+          <Foo name="green"/>
+          <Foo name="blue"/>
+        </div>
+        <div className="animals">
+          <Bar name="dog" />
+          <Bar name="cat" />
+          <Bar name="bird" />
+        </div>
+      </div>
+    );
+  }
+}
